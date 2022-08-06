e231ce5877d5882b50fdee6dac9e8ecaedf2fffa
Amirali <57327052+itsamirali@users.noreply.github.com>
2022-08-06 16:50:33 +0430

    added register component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { Box, Container } from "@mui/material";
 import "./App.css";
+import Register from "./Component/Register";
+import videoSrc from "./Static/master.m3u8"
 
 function App() {
   return (
-    <Container>
-      <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
-    </Container>
+    <div className="appMainContainer">
+        <video autoPlay loop muted width="352" height="198">
+            <source src={videoSrc} type="application/x-mpegURL"></source>
+        </video>
+      <Register />
+    </div>
   );
 }
 
