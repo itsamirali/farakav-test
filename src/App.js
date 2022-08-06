751757dab6d780c81022e3628d26df0f8758470f
Amirali <57327052+itsamirali@users.noreply.github.com>
2022-08-06 18:58:23 +0430

    installed react-hls-player

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import ReactHlsPlayer from "react-hls-player/dist";
 import "./App.css";
 import Register from "./Component/Register";
-import videoSrc from "./Static/master.m3u8"
+import videoSrc from "./Static/master.m3u8";
 
 function App() {
   return (
     <div className="appMainContainer">
-        <video autoPlay loop muted width="352" height="198">
-            <source src={videoSrc} type="application/x-mpegURL"></source>
-        </video>
+      <div className="videoContainer">
+        <ReactHlsPlayer
+          src={videoSrc}
+          width="100%"
+          height="auto"
+          autoPlay={true}
+          muted
+          controls={false}
+        />
+      </div>
       <Register />
     </div>
   );
